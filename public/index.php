5b1d9f22a50b8000a37c4ae87a61dc61a41d68cd
Ivan Fomin <la-verne@mail.ru>
2024-11-08 10:17:43 +0300

    Changed first update php version

--- public/index.php
+++ public/index.php
@@ -7,5 +7,5 @@ use Models\Article;
 $articles = Article::findLastArticles();
 
 foreach ($articles as $article) {
-    echo '<a href="/../templates/article.php?id=' . $article->id . '"> <h2>' . $article->title . '</h2></a>';
+    echo '<a href="/../templates/index.php?id=' . $article->id . '"> <h2>' . $article->title . '</h2></a>';
 }

116bf856f4aa6ce63ee328b94d0bd3b6aa90da10
Ivan Fomin <la-verne@mail.ru>
2024-11-16 20:35:33 +0300

    Change second level

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/autoload.php';
 use Models\Article;
 
 $articles = Article::findLastArticles();
-var_dump($articles);
 foreach ($articles as $article) {
     echo '<a href="/templates/index.php?id=' . $article->id . '"> <h2>' . $article->title . '</h2></a>';
 }
